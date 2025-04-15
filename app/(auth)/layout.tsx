6679378d30cc6f97690e52d4a266a4696d7f0c1c
sophia vincent <sophire33@gmail.com>
2025-04-15 04:40:19 -0700

    integrated light/dark mode for register/login form

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,12 @@
-const AuthLayout = ({children}: { children: React, ReactNode }) => {
+import ThemeToggler from "@/components/ThemeToggler";
+
+const AuthLayout = ({children}: { children: React.ReactNode }) => {
     return ( 
     <div className="h-[100vh] flex items-center
-    justify-center relative">
+    justify-center relative text-white">
+        <div className="absolute bottom-5 right-0">
+            <ThemeToggler />
+        </div>
         {children}
     </div> 
 );
